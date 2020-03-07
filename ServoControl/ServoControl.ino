f704d73112ec921612f540e7da72aef481aa3af4
Branden Wong <branden.wong1@ucalgary.ca>
2020-03-07 16:58:07 -0700

    Added some test functions

--- ServoControl/ServoControl.ino
+++ ServoControl/ServoControl.ino
@@ -1,10 +1,28 @@
+#include <Servo.h>
+#include <avr/sleep.h>
 
+#define SAMPLINGTIME 5000
+//900000 - 15 minutes
+//
+
+/**
+ * @author Branden Wong
+ *         Savipal Jessel
+ */
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(4, OUTPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(4, HIGH);
+  
 
+}
+
+void goSleep(){
+  sleep_enable();
+  set_sleep_mode(011);
 }
