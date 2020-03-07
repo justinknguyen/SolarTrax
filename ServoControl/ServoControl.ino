9875db8967098c18d353769cf58dfe00c3496ae7
Branden Wong <branden.wong1@ucalgary.ca>
2020-03-07 16:59:59 -0700

    small change

--- ServoControl/ServoControl.ino
+++ ServoControl/ServoControl.ino
@@ -2,8 +2,11 @@
 #include <avr/sleep.h>
 
 #define SAMPLINGTIME 5000
+#define INTERRUPTPIN 3
 //900000 - 15 minutes
 //
+
+volatile byte state = LOW;
 
 /**
  * @author Branden Wong
@@ -12,12 +15,15 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(4, OUTPUT);
+  pinMode(INTERRUPTPIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPTPIN), flash, CHANGE);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(4, HIGH);
+  digitalWrite(4, state);
+  goSleep();
   
 
 }
@@ -25,4 +31,8 @@ void loop() {
 void goSleep(){
   sleep_enable();
   set_sleep_mode(011);
+}
+
+void flash(){
+  state = !state;
 }
